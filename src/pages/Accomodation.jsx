9373a2f042cd92577b1dbc49fbb8dbe668d446dc
Thongkham Jean <juanitoo77550@hotmail.fr>
2022-10-07 18:56:30 +0200

    Added icon, updated SCSS

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -4,8 +4,9 @@ import Header from '../components/Header';
 
 const Accomodation = () => {
     return (
-        <div>
+        <div className="home__cards">
             <Header />
+
             <Footer />
         </div>
     );
