30abc11129f8a7e3f8fa52bf6991e6ca7c430f1a
Thongkham Jean <juanitoo77550@hotmail.fr>
2022-10-09 22:14:29 +0200

    updated collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -7,15 +7,15 @@ const Collapse = (props) => {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div>
-            <button className='collapse container' onClick={() => setIsOpen(!isOpen)}>
+        <div className='collapse container'>
+            <button className='collapse__button' onClick={() => setIsOpen(!isOpen)}>
                 <h3 className='collapse__title'>{props.title}</h3>
                 <FontAwesomeIcon icon={isOpen ? faAngleUp : faAngleDown} className='collapse__icon' />
             </button>
-            <div className='collapse__content'>
+            <div className='collapse__content' style={isOpen == true ? { height: "auto", backgroundColor: "#F6F6F6" } : { height: "0px" }}>
                 {props.content}
             </div>
-        </div>
+        </div >
     )
 }
 
