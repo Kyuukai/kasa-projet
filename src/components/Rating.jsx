83abd9c4473becfdc6c9a0c6516e141a4456ff31
Thongkham Jean <juanitoo77550@hotmail.fr>
2022-10-14 19:27:02 +0200

    Added ratings

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Rating = () => {
+const ColoredStar = () => {
     return (
-        <div>
+        <img src="../STAR.png" alt="Stars" />
+    )
+}
 
+const EmptyStar = () => {
+    return (
+        <img src="../EMPTY_STAR.png" alt="Empty Stars" />
+    )
+}
+
+const Rating = (props) => {
+
+    return (
+        <div className="rating">
+            {Array.from({ length: parseInt(props.rating) }, (v, idx) => <ColoredStar key={'colored-' + idx} />)}
+            {Array.from({ length: 5 - parseInt(props.rating) }, (v, idx) => <EmptyStar key={'empty-' + idx} />)}
         </div>
     );
 };
--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -3,14 +3,17 @@ import axios from 'axios';
 import AccommodationCollapse from '../components/AccommodationCollapse';
 import Tag from '../components/Tag';
 import { useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
 
 const Accommodation = () => {
     const [logement, setLogement] = useState({
         pictures: ["https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"],
         title: "",
         location: "",
+        host: ",",
         description: "",
         equipments: [],
+        rating: "",
         tags: []
     });
 
@@ -36,12 +39,27 @@ const Accommodation = () => {
                 alt={logement.title}
             />
             <div className="accommodation__header">
-                <div className="accommodation__header__title"><p>{logement.title}</p></div>
-                <div className="accommodation__header__location"><p>{logement.location}</p></div>
-                <div className="accommodation__header__tags">
+                <div className="accommodation__header__text">
+                    <div className="accommodation__header__text__title">{logement.title}</div>
+                    <div className="accommodation__header__text__location">{logement.location}</div>
+                </div>
+                <div className="accommodation__header__host">
+                    <div className="accommodation__header__host__hostName">{logement.host.name}</div>
+                    <img
+                        className="accommodation__header__host__hostPicture"
+                        src={logement.host.picture}
+                        alt={logement.host.name}
+                    />
+                </div>
+            </div>
+            <div className="accommodation__subheader">
+                <div className="accommodation__subheader__tags">
                     {logement.tags.map((tag) => (
                         <Tag key={logement.id + tag} tag={tag} />
                     ))}
+                </div>
+                <div className="accommodation__subheader__rating">
+                    <Rating key={logement.id} rating={logement.rating} />
                 </div>
             </div>
             <div className="accommodation__collapse">
